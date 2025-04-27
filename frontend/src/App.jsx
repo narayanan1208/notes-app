961055898fe738c100774ef4e68cc92fab89c671
narayanan1208 <narayanan6647@gmail.com>
2025-04-27 17:40:33 +0530

    Updated App with component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,26 @@
-function App() {
-  return (
-    <>
-      <div>App</div>
-    </>
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
+import MainLayout from "./layouts/MainLayout";
+import HomePage from "./pages/HomePage";
+import AddNotes from "./pages/AddNotes";
+import EditNotePage from "./pages/EditNotePage";
+import NotePage from "./pages/NotePage";
+
+const App = () => {
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route path="/" element={<MainLayout />}>
+        <Route index element={<HomePage />} />
+        <Route path="/add-notes" element={<AddNotes />} />
+      </Route>
+    )
   );
-}
+
+  return <RouterProvider router={router} />;
+};
 
 export default App;
