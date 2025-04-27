eedd8df0591b1004939c575a30d019b89b0f7386
narayanan1208 <narayanan6647@gmail.com>
2025-04-27 17:36:34 +0530

    Added HomePage component

---
+++ frontend/src/pages/HomePage.jsx
@@ -0,0 +1,15 @@
+import Filter from "../components/Filter";
+import NoteCardContainer from "../components/NoteCardContainer";
+
+import React from "react";
+
+const HomePage = () => {
+  return (
+    <>
+      <Filter />
+      <NoteCardContainer />
+    </>
+  );
+};
+
+export default HomePage;
