9849cfd7e2a1c40c8e9e3707da1aa39828e9ad01
narayanan1208 <narayanan6647@gmail.com>
2025-04-27 17:38:40 +0530

    Added NoteCardContainer component

---
+++ frontend/src/components/NoteCardContainer.jsx
@@ -0,0 +1,23 @@
+import NoteCard from "./NoteCard";
+
+const NoteCardContainer = () => {
+  return (
+    <div className="container">
+      <div className="note-has-grid row">
+        <NoteCard color="green" />
+        <NoteCard color="purple" />
+        <NoteCard color="blue" />
+
+        <NoteCard color="green" />
+        <NoteCard color="purple" />
+        <NoteCard color="blue" />
+
+        <NoteCard color="green" />
+        <NoteCard color="purple" />
+        <NoteCard color="blue" />
+      </div>
+    </div>
+  );
+};
+
+export default NoteCardContainer;
