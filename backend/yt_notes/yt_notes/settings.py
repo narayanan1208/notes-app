f8ba7ac9a142f6abb07be7f31f11b25202d618f3
narayanan1208 <narayanan6647@gmail.com>
2025-04-28 22:11:43 +0530

    Added cors in settings

--- backend/yt_notes/yt_notes/settings.py
+++ backend/yt_notes/yt_notes/settings.py
@@ -40,11 +40,13 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "noteapp",
     "rest_framework",
+    "corsheaders",
 ]
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
@@ -71,6 +73,9 @@ TEMPLATES = [
 
 WSGI_APPLICATION = "yt_notes.wsgi.application"
 
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5173",
+]
 
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
