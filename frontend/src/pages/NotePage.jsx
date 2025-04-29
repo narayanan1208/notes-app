7b3c33fe3d472b989283d7eac7f3fa772961cedc
narayanan1208 <narayanan6647@gmail.com>
2025-04-29 20:08:51 +0530

    Added API to fetch notes from backend

--- frontend/src/pages/NotePage.jsx
+++ frontend/src/pages/NotePage.jsx
@@ -1,22 +1,44 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./NotePage.css";
 import { BiSolidTrashAlt } from "react-icons/bi";
 import { FiEdit } from "react-icons/fi";
-import { Link } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 import Modal from "../components/Modal";
+import axios from "axios";
+import { FormatDate } from "../components/FormatDate";
 
 const NotePage = () => {
+  const [note, setNote] = useState({});
+  const { slug } = useParams();
+
+  useEffect(() => {
+    const fetchNote = async () => {
+      try {
+        const response = await axios.get(`http://localhost:8000/notes/${slug}`);
+        if (response.status !== 200) {
+          throw new Error("Failed to fetch notes");
+        }
+        const data = await response.data;
+        console.log("Notes:", data);
+        setNote(data);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    fetchNote();
+  }, [slug]);
+
   return (
     <>
       <div className="note-container">
-        <h3 className="title">REACT PROJECT FOR BEGINNERS</h3>
+        <h3 className="title">{note.title}</h3>
         <span className="d-flex justify-content-center">
           <p className="note-date font-12 text-muted me-5">
-            {" "}
-            created: 11 March 2009
+            created: {FormatDate(note.created)}
           </p>
           <p className="note-date font-12 text-muted me-5">
-            last updated: 11 March 2009
+            last updated: {FormatDate(note.updated)}
           </p>
         </span>
         <span className="button-group">
@@ -32,19 +54,7 @@ const NotePage = () => {
           </button>
         </span>
         <p className="description">
-          Sed ut perspiciatis unde omnis iste natus error sit voluptatem
-          accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae
-          ab illo inventore veritatis et quasi architecto beatae vitae dicta
-          sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit
-          aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos
-          qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui
-          dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed
-          quia non numquam eius modi tempora incidunt ut labore et dolore magnam
-          aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum
-          exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex
-          ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in
-          ea voluptate velit esse quam nihil molestiae consequatur, vel illum
-          qui dolorem eum fugiat quo voluptas nulla pariatur?
+          {note.body}
         </p>
       </div>
       <Modal />
