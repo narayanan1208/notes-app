e55694bb5ce6b0bd60c0d534f9aa5aa13cdfe627
narayanan1208 <narayanan6647@gmail.com>
2025-06-29 01:17:59 +0530

    Added search functionality in backend

--- backend/yt_notes/noteapp/urls.py
+++ backend/yt_notes/noteapp/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path("notes/", views.notes, name="notes"),
     path("notes/<slug:slug>/", views.note_detail, name="note-detail"),
+    path("notes-search/", views.search_notes, name="notes-search"),
 ]
--- backend/yt_notes/noteapp/views.py
+++ backend/yt_notes/noteapp/views.py
@@ -3,10 +3,23 @@ from noteapp.models import Note
 from noteapp.serializers import NoteSerializer
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
+from django.db.models import Q
 from rest_framework import status
 
 
 # Create your views here.
+@api_view(["GET"])
+def search_notes(request):
+    query = request.query_params.get("search")
+    notes = Note.objects.filter(
+        Q(title__icontains=query)
+        | Q(body__icontains=query)
+        | Q(category__icontains=query)
+    )
+    serializer = NoteSerializer(notes, many=True)
+    return Response(serializer.data, status=status.HTTP_200_OK)
+
+
 @api_view(["GET", "POST"])
 def notes(request):
     if request.method == "GET":
